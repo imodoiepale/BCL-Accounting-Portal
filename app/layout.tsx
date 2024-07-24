30743d2a4d3aefc0c215f2f8fadf7cba093c8c86
imodoiepale <ijepale@gmail.com>
2024-07-24 10:19:38 +0300

    clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,13 +17,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
+      <header>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
         <div className="flex gap-4">
           <Sidebar/>
           {children}
         </div>
         </body>
     </html>
+    </ClerkProvider>
   );
 }
