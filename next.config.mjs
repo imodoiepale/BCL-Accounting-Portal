1d0970d1cf3e7ce81b745554edaecba4dff0c1ac
imodoiepale <ijepale@gmail.com>
2024-07-25 09:56:30 +0300

    yeah

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import { config as dotenvConfig } from 'dotenv';
+
+// Load environment variables from .env.local
+dotenvConfig();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  },
+};
 
 export default nextConfig;
