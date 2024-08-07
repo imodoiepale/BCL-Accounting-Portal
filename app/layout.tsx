5acd70ed22ffe98b3c6953d99e6ec4bb96899e91
imodoiepale <ijepale@gmail.com>
2024-08-07 13:49:26 +0300

    yeah

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex gap-4">
+        <div className="flex gap-4" style={{ transform: 'scale(0.8)', transformOrigin: 'top left', minHeight: '125vh', width: '125%' }}>
           <Sidebar/>
           <div className="flex flex-col w-full"> {/* Full height column */}
             <Navbar/>
