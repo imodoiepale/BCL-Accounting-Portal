11c82a0267b643bb097f773dadafd391a74bf17b
imodoiepale <ijepale@gmail.com>
2024-08-15 11:22:31 +0300

    REPORTS

--- components/component/pettycash/pettycash.tsx
+++ components/component/pettycash/pettycash.tsx
@@ -14,6 +14,7 @@ import UsersTab from './users';
 import AccountsTab from './accounts';
 import { TransactionsTab } from './entries';
 import PettyCashSettings from './settings';
+import PettyCashReportsTab from './reports';
 
 const supabase = createClient('https://zyszsqgdlrpnunkegipk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5c3pzcWdkbHJwbnVua2VnaXBrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwODMyNzg5NCwiZXhwIjoyMDIzOTAzODk0fQ.7ICIGCpKqPMxaSLiSZ5MNMWRPqrTr5pHprM0lBaNing');
 
@@ -148,6 +149,9 @@ export function PettyCashManager() {
           <TabsContent value="transactions">
             <TransactionsTab settings={settings.transactions} />
           </TabsContent>
+          <TabsContent value="reports">
+          <PettyCashReportsTab />
+        </TabsContent>
         </Tabs>
       </main>
     </div>
