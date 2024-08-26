5dd1d6657497f4b8ef8c4ccf682fe37631544c13
johnnalwa <johnneyjayson@gmail.com>
2024-08-26 09:08:14 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -19,17 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="flex gap-4" style={{ transform: 'scale(0.8)', transformOrigin: 'top left', minHeight: '125vh', width: '125%' }}>
-          <Sidebar/>
-          <div className="flex flex-col w-full"> {/* Full height column */}
-            <Navbar/>
-              {children}
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="flex h-screen overflow-hidden">
+            <Sidebar />
+            <div className="flex flex-col flex-grow">
+              <Navbar />
+              <main className="flex-grow overflow-auto p-4">
+                {children}
+              </main>
+            </div>
           </div>
-        </div>
         </body>
-    </html>
+      </html>
     </ClerkProvider>
   );
 }
