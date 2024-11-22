c0879301ab94518650a7418ce69ea92a8e0fe989
imodoiepale <ijepale@gmail.com>
2024-11-22 17:30:53 +0300

    FBV

--- app/admin/template/page.tsx
+++ app/admin/template/page.tsx
@@ -2,20 +2,30 @@ import TemplateSelector from './components/TemplateSelector'
 import FormBuilder from './components/FormBuilder'
 import DocumentPreview from './components/DocumentPreview'
 import TemplateUploader from './components/TemplateUploader'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Home() {
   return (
     <div className=" mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Form Template Generator</h1>
-        <TemplateUploader/>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <div>
-          <TemplateSelector />
-          <FormBuilder />
-        </div>
-        <DocumentPreview />
-      </div>
+      <Tabs defaultValue="generate" className="w-full">
+        <TabsList>
+          <TabsTrigger value="generate">Generate Document</TabsTrigger>
+          <TabsTrigger value="settings">Template Settings</TabsTrigger>
+        </TabsList>
+        <TabsContent value="generate">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            <div>
+              <TemplateSelector />
+              <FormBuilder />
+            </div>
+            <DocumentPreview />
+          </div>
+        </TabsContent>
+        <TabsContent value="settings">
+          <TemplateUploader />
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
-
